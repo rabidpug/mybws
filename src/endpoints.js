c332d9fe6f20eb12bf8c9eff41d60bb84f5ce28f
Matt Cuneo <m@jcuneo.com>
2018-07-12 02:23:40 +1000

    Fixed subscriptionEndpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -2,5 +2,4 @@ import { API_ENDPOINT, } from './env'
 
 export const authEndpoint = path => `${API_ENDPOINT}/auth/${path || ''}`
 export const graphqlEndpoint = () => `${API_ENDPOINT}/v1`
-export const subscriptionEndpoint = () =>
-  `${API_ENDPOINT.replace( /^(.*)s{0,1}:\/\//, ( m, p ) => m.replace( p, 'ws' ) )}/graphql`
+export const subscriptionEndpoint = () => `${API_ENDPOINT.replace( 'http', 'ws' )}/graphql`
